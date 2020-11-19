a9e0cd577391ff26cc5862c8869bd4e04961d718
skanagui <s.kanagui@gmail.com>
2020-11-19 12:06:33 -0500

    syntax set me back

--- src/Component/Character.js
+++ src/Component/Character.js
@@ -5,8 +5,11 @@ function Character ({char}){
 
     return (
         <>
-    <h2>{char.name}</h2>
-    <img alt = "character" src = {char.img}/>
+            <h2>{char.name}</h2>
+            <img alt = "character" src = {char.img}/>
+            <h3>{char.show}</h3>
+            <br></br>
+            <br></br>
 
         </>
 
